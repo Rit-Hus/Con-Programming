a6148f83283f0fa01f3cdd4bace8918e62901919
Ritwan Hussein <ritwan.hussein@hotmail.com>
2026-02-02 21:50:20 +0100

    Sort(done)

---
+++ HW2/Task2/sort.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <sys/time.h>
+#include <stdbool.h>
+
+const int THRESHOLD = 1000;  
+const int POOLSIZE = 1000000;
+const int MAXCOUNT = 8;
+
+
+void swap(int* array, int left, int right){
+    int temp = array[right];
+    array[right] = array[left];
+    array[left] = temp;
+}
+
+int partition(int* array, int low, int high){
+    int pivot = array[low];
+    int wall = low;
+    int i = low + 1;
+    while(i <= high){
+        if(array[i] < pivot){
+            wall++;
+            swap(array, i, wall);
+        }
+        i++;
+    }
+    swap(array, low, wall);
+    return wall;
+}
+
+void* quicksort(int* array, int low, int high){
+   
+   if (high - low > THRESHOLD) {
+        int pivot_index = partition(array, low, high);
+        if (high - low > THRESHOLD) {
+            #pragma omp task
+            quicksort(array, low, pivot_index - 1);
+
+            #pragma omp task
+            quicksort(array, pivot_index + 1, high);
+
+            #pragma omp taskwait
+        } else {
+            quicksort(array, low, pivot_index - 1);
+            quicksort(array, pivot_index + 1, high);
+        }
+    }
+}
+
+
+int main(){
+    double times[5];
+    int *unsortedData = malloc(sizeof(int) * POOLSIZE);
+    omp_set_num_threads(MAXCOUNT);
+    printf("Testing with %d processors and %d elements...\n", MAXCOUNT, POOLSIZE);
+
+    for (int run = 0; run < 5; run++) {
+      
+        for (int i = 0; i < POOLSIZE; i++) unsortedData[i] = rand() % 100000;
+
+        double start_time = omp_get_wtime();
+
+        #pragma omp parallel
+        {
+            #pragma omp single
+            quicksort(unsortedData, 0, POOLSIZE - 1);
+        }
+
+        times[run] = omp_get_wtime() - start_time;
+        printf("Run %d: %g sec\n", run + 1, times[run]);
+    }
+
+    // Sort the 5 times
+    for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4 - i; j++) {
+        if (times[j] > times[j + 1]) {
+            double temp = times[j];
+            times[j] = times[j + 1];
+            times[j + 1] = temp;
+        }
+    }
+    }
+    printf("\nMEDIAN EXECUTION TIME: %g sec\n", times[2]);
+    free(unsortedData);
+    return 0;
+}
