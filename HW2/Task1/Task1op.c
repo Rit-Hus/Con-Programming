a15edcf667e72b8996218df74dde4e167c284ed5
Astrososa-2004 <samiee@kth.se>
2026-02-04 12:21:16 +0100

    removed 'HW2/Task1op.c' and modified 'HW2/Task1

--- HW2/Task1/Task1op.c
+++ HW2/Task1/Task1op.c
@@ -7,22 +7,27 @@
 */
 
 #include <omp.h>
-
-double start_time, end_time;
-
+#include <time.h>
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define MAXSIZE 10000  /* maximum matrix size */
 #define MAXWORKERS 8   /* maximum number of workers */
+
+
+
 
 int numWorkers;
 int size; 
 int matrix[MAXSIZE][MAXSIZE];
 void *Worker(void *);
+double start_time, end_time;
 
 /* read command line, initialize, and create threads */
 int main(int argc, char *argv[]) {
-  int i, j, total=0;
-
+  int i, j;
+  long long total = 0;
+srand(time(NULL));
   /* read command line args if any */
   size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
   numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
@@ -41,18 +46,95 @@ int main(int argc, char *argv[]) {
 	  //	  printf(" ]\n");
   }
 
+int globalMin = INT_MAX,  globalMinRow = 0, globalMinCol = 0;
+int globalMax = INT_MIN, globalMaxRow = 0, globalMaxCol = 0;
+
   start_time = omp_get_wtime();
-#pragma omp parallel for reduction (+:total) private(j)
+  #pragma omp parallel 
+  {
+int localMin = matrix[0][0];
+int localMinRow = 0, localMinCol = 0;
+int localMax = matrix[0][0];
+int localMaxRow = 0, localMaxCol = 0;
+
+
+
+#pragma omp  for reduction (+:total) private(j)
   for (i = 0; i < size; i++)
     for (j = 0; j < size; j++){
       total += matrix[i][j];
-    }
-// implicit barrier
+int v = matrix[i][j];
 
-  end_time = omp_get_wtime();
-
-  printf("the total is %d\n", total);
-  printf("it took %g seconds\n", end_time - start_time);
-
+      
+    
+    
+    if(v < localMin) {
+    localMin = v;
+    localMinRow = i;
+    localMinCol = j;
 }
 
+if(v > localMax) {
+    localMax = v;
+    localMaxRow = i;
+    localMaxCol = j;
+}
+
+
+
+
+
+    }
+
+
+
+
+#pragma omp critical
+{
+
+if(localMin < globalMin) {
+    globalMin = localMin;
+    globalMinRow = localMinRow;
+    globalMinCol = localMinCol;
+}
+if (localMax > globalMax) {
+    globalMax = localMax;
+    globalMaxRow = localMaxRow;
+    globalMaxCol = localMaxCol;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+ }
+
+end_time = omp_get_wtime();
+
+  printf("the total is %lld\n", total);
+  printf("it took %g seconds\n", end_time - start_time);
+  printf("Global Min: %d at (%d, %d)\n", globalMin, globalMinRow, globalMinCol);
+  printf("Global Max: %d at (%d, %d)\n", globalMax, globalMaxRow, globalMaxCol);
+
+
+
+}
+// implicit barrier
+
+ 
+
+
+
+
+
+
+ 
+ 
