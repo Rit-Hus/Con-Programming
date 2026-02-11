81ad2d02ba006037f17edece9d40cc7fee579cdc
Astrososa-2004 <samiee@kth.se>
2026-02-11 18:32:38 +0100

    removed timnestamp

--- HW3/Task2/Task2.c
+++ HW3/Task2/Task2.c
@@ -76,20 +76,6 @@ sem_post(&empty);
 return NULL;
 }
 
-double read_timer() {
-    static bool initialized = false;
-    static struct timeval start;
-    struct timeval end;
-    if( !initialized )
-    {
-        gettimeofday( &start, NULL );
-        initialized = true;
-    }
-    gettimeofday( &end, NULL );
-    return (end.tv_sec - start.tv_sec) + 1.0e-6 * (end.tv_usec - start.tv_usec);
-}
-
-double start_time, end_time;
 
 
 
@@ -115,7 +101,6 @@ pthread_t bear_thread;
     pthread_t bee_threads[BEES];
     int ids[BEES];
 
-  start_time = read_timer();
 
     pthread_create(&bear_thread, NULL, bear, NULL);
 
@@ -129,8 +114,6 @@ pthread_create(&bee_threads[i], NULL, bee, &ids[i]);
 
 
 pthread_join(bear_thread, NULL);
-    end_time = read_timer();
-printf("Execution time: %g seconds\n", end_time - start_time);
 
 return 0;   
 
