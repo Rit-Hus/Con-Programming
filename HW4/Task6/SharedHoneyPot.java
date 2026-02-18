9b92535302167bf5f2f9034e17454bd56f822b8c
Astrososa-2004 <samiee@kth.se>
2026-02-18 12:39:05 +0100

    added comments

--- HW4/Task6/SharedHoneyPot.java
+++ HW4/Task6/SharedHoneyPot.java
@@ -10,11 +10,12 @@ public Bee(SharedHoneyPot HoneyPot) {
 
 
 }
+// Method to signal the bee to stop producing honey
 public void setStop(){
     this.stop = true;
 
 }
-
+// Method to check if the bee should stop producing honey
 public void run() {
 
     int honey = 1;
@@ -23,7 +24,7 @@ while (!stop){
 HoneyPot.addHoney(honey);
 
 
-
+// Simulate time taken to produce honey
 try {
   sleep (1000);
 } catch (InterruptedException e) {}
@@ -40,6 +41,7 @@ public Bear(SharedHoneyPot HoneyPot) {
     this.HoneyPot = HoneyPot;
 }
 
+// Method to consume honey from the pot
 public void run() {
     while (true) {
         HoneyPot.consumeHoney();
@@ -54,16 +56,26 @@ public void run() {
 
 
 
+/*
+ * Fairness:
+ * This solution is correct but not strictly fair. We use Java's intrinsic monitor
+ * (synchronized) with wait()/notifyAll(). When notifyAll() is called, all waiting
+ * threads wake up, but the JVM/OS scheduler decides which thread re-acquires the
+ * monitor lock next. There is no FIFO guarantee, so a particular bee could in
+ * theory be postponed repeatedly (starvation is possible, though unlikely in practice).
+ */
 
-  public class SharedHoneyPot {
 
+public class SharedHoneyPot {
+
+    // Shared resource: the honey pot
 private int honey = 0;
 private int capacity = 15;
 
 
  public synchronized  void addHoney(int amount){
      
-
+// If the pot is full, wait until the bear consumes the honey
 while(honey >= capacity){
 
     try {
@@ -76,10 +88,14 @@ while(honey >= capacity){
     honey += amount;
 System.out.println(" Bee" + Thread.currentThread().getName() +"added " + amount + " units of honey. Total: " + honey);
 
+
+// If the pot is full after adding honey, notify the bear to consume it
 notifyAll();
 
 }
 
+
+// Method for the bear to consume honey from the pot
 public synchronized void consumeHoney(){
     while(honey < capacity){
         try {
@@ -91,14 +107,6 @@ public synchronized void consumeHoney(){
         honey = 0;
     notifyAll();
 System.out.println("Honey consumed, remaining: " + honey);
-
-
-    
-
-
-
-
-
 
 
 
